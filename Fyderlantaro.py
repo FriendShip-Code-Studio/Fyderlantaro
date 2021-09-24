49d5ed41ff47415a0e425ec7d2dd4bfdebf1a16b
AsakiRain <1287059631@qq.com>
2021-09-24 22:16:18 +0800

    bring auth identity autosave feature

--- Fyderlantaro.py
+++ Fyderlantaro.py
@@ -2,7 +2,7 @@ from ftplib import FTP
 import time
 import platform
 import os
-
+import json
 
 if platform.system() == 'Windows':
     clear = 'cls'
@@ -11,18 +11,48 @@ else:
 
 print("欢迎使用 Fyderlantaro | Powered by FriendShip Studio")
 print("[C] FriendShip Studio 2021     All rights reserved.")
-server_address = input("请输入FTP服务器地址:\n")
-
-ctrl = True
-while ctrl == True:
+config_path = os.path.join((os.path.dirname(os.path.abspath(__file__)) + os.path.sep),"config.json")
+if os.path.exists(config_path):
+    print("============================================================")
+    print("[INFO] 检测到目录下的config.json，读取...",end="")
+    f_config = open(config_path)
+    print("完成")
+    print("[INFO] 加载配置...",end="")
     try:
-        server_port = int(input("请输入服务器的端口:\n"))
-        ctrl = False
-    except TypeError:
-        print("错误的端口数据，请重新输入")
+        config = json.load(f_config)
+        server_address = config['server_address']
+        server_port = config['server_port']
+        username = config['username']
+        password = config['password']
+    except Exception as e:
+        f_config.close()
+        skip_input = False
+        print("失败")
+        os.remove(config_path)
+        print("[INFO] 请重启程序，重新登录")
+        print("============================================================")
+    f_config.close()
+    if all([server_address,server_port,username,password]):
+        print("成功")
+        print("============================================================")
+        skip_input = True
+    else:
+        skip_input = False
+else:
+    skip_input = False
+if skip_input == False:
+    server_address = input("请输入FTP服务器地址:\n")
 
-username = input("请输入服务器登录用户名:\n")
-password = input("请输入服务器登录密码:\n")
+    ctrl = True
+    while ctrl == True:
+        try:
+            server_port = int(input("请输入服务器的端口:\n"))
+            ctrl = False
+        except TypeError:
+            print("错误的端口数据，请重新输入")
+
+    username = input("请输入服务器登录用户名:\n")
+    password = input("请输入服务器登录密码:\n")
 
 ftp = FTP()
 ftp.set_debuglevel(0)
@@ -48,6 +78,16 @@ except:
 
 print("============================================================")
 print("[INFO] 成功与服务器建立连接")
+print("[INFO] 储存登录凭据...",end="")
+with open(config_path,'w') as f:
+    config = {
+            'server_address': server_address,
+            'server_port': server_port,
+            'username': username,
+            'password': password,
+            }
+    json.dump(config,f)
+print("完成")
 print("============================================================")
 time.sleep(1.0)
 ftp.encoding = 'utf-8'
@@ -75,7 +115,7 @@ while ctrl == True:
     elif branch == 'quit':
         os.system(clear)
         print("感谢使用Fyderlantaro | 欢迎访问 https://github.com/FriendShip-Code-Studio")
-        time.sleep(3)
+        time.sleep(1)
         exit(0)
     elif branch.startswith("cd "):
         ch_dir = branch[3:]
